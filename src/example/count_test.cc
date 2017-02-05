93eea103392b51667c0ae4b09703a33ca068eaeb
Michael Whittaker <mjw297@cornell.edu>
2017-02-05 00:32:44 +0000

    Added Google's logging library glog.

--- src/example/count_test.cc
+++ src/example/count_test.cc
@@ -1,16 +1,21 @@
 #include "example/count.h"
 
+#include <cstddef>
+
 #include <vector>
 
+#include "glog/logging.h"
 #include "gtest/gtest.h"
 #include "range/v3/all.hpp"
 
 namespace example {
 
 TEST(Count, SimpleRanges) {
+  LOG(INFO) << "Running SimpleRanges test.";
   std::vector<int> zero = {};
   std::vector<int> one = {0};
   std::vector<int> five = {0, 0, 0, 0, 0};
+  CHECK_EQ(five.size(), static_cast<std::size_t>(5)) << "5 != 5";
 
   EXPECT_EQ(0, Count(ranges::view::all(zero)));
   EXPECT_EQ(1, Count(ranges::view::all(one)));
@@ -28,6 +33,7 @@ TEST(Count, ComplexRanges) {
 }  // namespace example
 
 int main(int argc, char **argv) {
+  google::InitGoogleLogging(argv[0]);
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }

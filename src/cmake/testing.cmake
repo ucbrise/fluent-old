0fa59dfcc84f168319f15029846b81e6ddfaa241
Michael Whittaker <mjw297@cornell.edu>
2017-04-17 11:57:33 -0700

    Made tests run again.
    A previous commit put all executables into a bin directory. This
    prevented tests from running, but I didn't realize it until now.

--- src/cmake/testing.cmake
+++ src/cmake/testing.cmake
@@ -1,10 +1,13 @@
 CMAKE_MINIMUM_REQUIRED(VERSION 3.0)
 
 # CREATE_NAMED_TEST(foo bar.cc) creates a test named `foo` from the file
-# `bar.cc`.
+# `bar.cc`. Refer to [1] and [2] for documentation on how ADD_TEST works.
+#
+# [1]: http://stackoverflow.com/a/21413672/3187068
+# [2]: https://cmake.org/cmake/help/v3.0/command/add_test.html
 MACRO(CREATE_NAMED_TEST NAME FILENAME)
     ADD_EXECUTABLE(${NAME} ${FILENAME})
-    ADD_TEST(${NAME} ${NAME})
+    ADD_TEST(NAME ${NAME} COMMAND ${NAME})
     ADD_DEPENDENCIES(${NAME} googletest)
     TARGET_LINK_LIBRARIES(${NAME}
         ${GTEST_LIBS_DIR}/libgtest.a

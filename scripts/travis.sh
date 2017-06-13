0cd78fd91d2b87fe2d1525d3b383130b70d9ca63
Michael Whittaker <mjw297@cornell.edu>
2017-06-13 10:08:14 -0700

    Reduced parallelism of build.

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 main() {
     rm -rf build && \
-    ./scripts/build.sh "$TEST_TYPE" 8 && \
+    ./scripts/build.sh "$TEST_TYPE" 2 && \
     (cd build && ctest -L UNITTEST) && \
     ./scripts/run_benchmarks.sh
 }

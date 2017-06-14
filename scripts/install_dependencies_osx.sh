1466962293975423485937051ebfd8f08347d801
Joe Hellerstein <jmh@berkeley.edu>
2017-06-14 10:21:51 -0700

    remove dead code

--- scripts/install_dependencies_osx.sh
+++ scripts/install_dependencies_osx.sh
@@ -6,42 +6,9 @@ install_misc() {
     brew install libtool
 }
 
-# install_clang() {
-#     sudo bash -c "echo '' >> /etc/apt/sources.list"
-#     sudo bash -c "echo '# http://apt.llvm.org/' >> /etc/apt/sources.list"
-#     sudo bash -c "echo 'deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-3.9 main' >> /etc/apt/sources.list"
-#     sudo bash -c "echo 'deb-src http://apt.llvm.org/trusty/ llvm-toolchain-trusty-3.9 main' >> /etc/apt/sources.list"
-#     wget -O - http://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
-#     sudo apt-get -y update
-
-#     sudo apt-get install -y \
-#         clang-3.9 clang-format-3.9 clang-tidy-3.9 lldb-3.9
-#     sudo ln -s "$(which clang-3.9)" /usr/bin/clang
-#     sudo ln -s "$(which clang++-3.9)" /usr/bin/clang++
-#     sudo ln -s "$(which clang-format-3.9)" /usr/bin/clang-format
-#     sudo ln -s "$(which clang-tidy-3.9)" /usr/bin/clang-tidy
-#     sudo ln -s "$(which lldb-3.9)" /usr/bin/lldb
-#     sudo ln -s "$(which lldb-server-3.9)" /usr/bin/lldb-server
-# }
-
-# install_gpp() {
-#     sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-#     sudo apt-get update
-#     sudo apt-get install -y g++-6
-#     sudo ln -sf "$(which /usr/bin/g++-6)" /usr/bin/g++
-#     sudo ln -sf "$(which /usr/bin/gcc-6)" /usr/bin/gcc
-# }
-
 install_cmake() {
     brew install cmake
 }
-
-# install_postgres() {
-#     sudo bash -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-#     wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-#     sudo apt-get update
-#     sudo apt-get install -y postgresql-9.6 postgresql-server-dev-9.6 python-dev
-# }
 
 install_redis() {
     brew install libhiredis libev

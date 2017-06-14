d6f289f7b18564857a9c5dfe3008f87be49a2817
Joe Hellerstein <jmh@berkeley.edu>
2017-06-14 09:52:31 -0700

    pass TEST_TYPE into run_benchmarks.sh

--- scripts/run_benchmarks.sh
+++ scripts/run_benchmarks.sh
@@ -2,9 +2,24 @@
 
 set -euo pipefail
 
+usage() {
+    echo "run_benchmarks.sh <Debug|Release>"
+    exit 1
+}
+
 main() {
+	if [[ $# -ne 1 && $# -ne 2 ]]; then
+		echo "Wrong count" $#
+        usage
+    fi
+
+    build_type="$1"
+    if [[ "$build_type" != "Debug" && "$build_type" != "Release" ]]; then
+        usage
+    fi
+
     first=true
-    for bench in build/bin/*_bench; do
+    for bench in build/${build_type}/bin/*_bench; do
         if "$first"; then
             "$bench" --benchmark_format=csv 2>/dev/null
             first=false
@@ -14,4 +29,4 @@ main() {
     done
 }
 
-main
+main "$@"

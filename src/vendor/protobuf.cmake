31607ce9208e9c6a76a47ef5fd42ee0d6650d990
Michael Whittaker <mjw297@cornell.edu>
2017-06-17 19:45:13 -0700

    Trying to get protobuf working from a fresh build.

--- src/vendor/protobuf.cmake
+++ src/vendor/protobuf.cmake
@@ -11,6 +11,16 @@ ExternalProject_Add(protobuf_project
     INSTALL_COMMAND ""
 )
 
+# We'd like to find the protobuf version we install above, but unfortunately
+# this is hard to do without jumping through hoops [1]. Instead, we run
+# FIND_PACKAGE(Protobuf) to get the PROTOBUF_GENERATE_CPP command, but then
+# override all the relevant variables.
+#
+# [1]: https://cmake.org/pipermail/cmake/2013-October/056105.html
+SET(Protobuf_DEBUG true)
+INCLUDE(FindProtobuf)
+FIND_PACKAGE(Protobuf)
+
 SET(PROTOBUF_INCLUDE_DIRS
     ${CMAKE_CURRENT_BINARY_DIR}/src/protobuf_project/src)
 SET(PROTOBUF_LINK_DIRS
@@ -20,5 +30,3 @@ SET(Protobuf_SRC_ROOT_FOLDER
     ${CMAKE_CURRENT_BINARY_DIR}/src/protobuf_project)
 SET(Protobuf_PROTOC_EXECUTABLE
     ${CMAKE_CURRENT_BINARY_DIR}/src/protobuf_project/src/protoc)
-INCLUDE(FindProtobuf)
-FIND_PACKAGE(Protobuf)

f2f67b1ec589c2a7595e096be97f7ef747d83f4b
Michael Whittaker <mjw297@cornell.edu>
2017-06-22 11:31:29 -0700

    Fixed travis bug not running tests.

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 main() {
     rm -rf build && \
     ./scripts/build.sh "$TEST_TYPE" 1 && \
-    (cd build && ctest -L UNITTEST) && \
+    (cd build/"$TEST_TYPE" && ctest -L UNITTEST) && \
     ./scripts/run_benchmarks.sh "$TEST_TYPE"
 }
 

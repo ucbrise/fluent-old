057fb0b41a3ce04cea542ec3c2889f4b842a40ab
Michael Whittaker <mjw297@cornell.edu>
2017-06-22 21:12:03 -0700

    Finished distributed key-value store.

--- src/examples/distributed_key_value_store/client.h
+++ src/examples/distributed_key_value_store/client.h
@@ -13,13 +13,16 @@
 class KeyValueServiceClient {
  public:
   KeyValueServiceClient(std::shared_ptr<grpc::Channel> channel)
-      : stub_(KeyValueService::NewStub(channel)) {}
+      : channel_(channel), stub_(KeyValueService::NewStub(channel)) {}
 
   bool Set(const std::string& key, const google::protobuf::int64 value);
   google::protobuf::int64 Get(const std::string& key);
   bool Merge(const std::map<std::string, google::protobuf::int64>& kvs);
 
+  std::shared_ptr<grpc::Channel> Channel() { return channel_; }
+
  private:
+  std::shared_ptr<grpc::Channel> channel_;
   std::unique_ptr<KeyValueService::Stub> stub_;
 };
 
---
+++ src/examples/distributed_key_value_store/launch_distributed_key_value_store.sh
@@ -0,0 +1,34 @@
+#! /usr/bin/env bash
+
+set -euo pipefail
+
+main() {
+    # http://stackoverflow.com/a/13864829/3187068
+    if [[ -z ${TMUX+dummy} ]]; then
+        echo "ERROR: you must run this script while in tmux."
+        return 1
+    fi
+
+    session="$(tmux display-message -p '#S')"
+    tmux new-window -t "$session" -n "dkvs"
+    tmux split-window -h
+    tmux split-window -h
+    tmux split-window -h
+    tmux select-layout main-vertical
+    tmux select-pane -t 0
+
+    glog="GLOG_logtostderr=1"
+    bin="./build/Debug/bin"
+    server="$bin/examples_distributed_key_value_store_server"
+    client="$bin/examples_distributed_key_value_store_command_line_client"
+    addr9000="0.0.0.0:9000"
+    addr9001="0.0.0.0:9001"
+    addr9002="0.0.0.0:9002"
+
+    tmux send-keys -t 1 "$glog $server ${addr9000} ${addr9001} ${addr9002}" C-m
+    tmux send-keys -t 2 "$glog $server ${addr9001} ${addr9002} ${addr9000}" C-m
+    tmux send-keys -t 3 "$glog $server ${addr9002} ${addr9000} ${addr9001}" C-m
+    tmux send-keys -t 0 "$glog $client ${addr9000}" C-m
+}
+
+main

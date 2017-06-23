e63b0e91092c1df0a5266db1fecff5d83c067a2c
Michael Whittaker <mjw297@cornell.edu>
2017-06-22 17:50:53 -0700

    Added a RandInt function.

---
+++ src/common/rand_util_test.cc
@@ -0,0 +1,31 @@
+#include "common/rand_util.h"
+
+#include "glog/logging.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+namespace fluent {
+
+using ::testing::AllOf;
+using ::testing::Ge;
+using ::testing::Lt;
+
+TEST(RandUtil, RandInt) {
+  const int num_trials = 100;
+  for (int low = 0; low < 5; ++low) {
+    for (int high = low + 1; high < 5; ++high) {
+      for (int i = 0; i < num_trials; ++i) {
+        int x = RandInt(low, high);
+        EXPECT_THAT(x, AllOf(Ge(low), Lt(high)));
+      }
+    }
+  }
+}
+
+}  // namespace fluent
+
+int main(int argc, char **argv) {
+  google::InitGoogleLogging(argv[0]);
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}

2cbc624fcb9c05724433b553c7a7f0483d9f2e35
Michael Whittaker <mjw297@cornell.edu>
2017-06-27 17:07:10 -0700

    Added Cereal pickler.

---
+++ src/vendor/cereal.cmake
@@ -0,0 +1,13 @@
+ExternalProject_Add(cereal_project
+    GIT_REPOSITORY "https://github.com/USCiLab/cereal"
+    GIT_TAG "v1.2.2"
+    PREFIX ${CMAKE_CURRENT_BINARY_DIR}
+    BUILD_IN_SOURCE 1
+    UPDATE_COMMAND ""
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
+)
+
+ExternalProject_Get_Property(cereal_project SOURCE_DIR)
+SET(CEREAL_INCLUDE_DIRS ${SOURCE_DIR}/include)

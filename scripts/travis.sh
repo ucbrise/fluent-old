077fa5cfac721c7f251cf18c620ff1904b9e8c71
Michael Whittaker <mjw297@cornell.edu>
2017-07-02 14:42:59 -0700

    Trying to appease to the travis gods.

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -6,13 +6,8 @@ main() {
     # Remove the build directory just in case there's something there already.
     rm -rf build/"$TEST_TYPE";
 
-    # Build the code. Sometimes, building a fluent program can take a really,
-    # really long time. This triggers travis to fail because the compiler runs
-    # for quite a while with no ouput. We use the travis_wait command [1] to
-    # prevent travis from prematurely killing our jobs.
-    #
-    # [1]: https://goo.gl/XAqkJg
-    travis_wait 60 ./scripts/build.sh "$TEST_TYPE" 1;
+    # Build the code.
+    ./scripts/build.sh "$TEST_TYPE" 1;
 
     # Run all the unit tests.
     (cd build/"$TEST_TYPE" && ctest -L UNITTEST);

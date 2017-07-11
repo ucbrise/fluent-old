753f72f1dd4658fe0180ed21c8e520ec36f69429
Michael Whittaker <mjw297@cornell.edu>
2017-07-11 01:59:23 +0000

    Tidied redis bench.

--- src/examples/redis/speed_test_sync.cc
+++ src/examples/redis/speed_test_sync.cc
@@ -12,23 +12,19 @@ int main() {
     return 1;
   }
 
-  std::cout << "Sending SET synchronously for 5 seconds." << std::endl;
-
   using namespace std::chrono;
   nanoseconds duration = seconds(5);
   time_point<system_clock> start = system_clock::now();
   time_point<system_clock> stop = start + duration;
   int count = 1;
   for (count = 1; system_clock::now() < stop; ++count) {
-    rdx.set("k", "1");
+    CHECK(rdx.set("k", "42"));
   }
 
   nanoseconds elapsed = system_clock::now() - start;
   double seconds = elapsed.count() / 1e9;
   double frequency = static_cast<double>(count - 1) / seconds;
-  std::cout << (count - 1) << " commands" << std::endl;
-  std::cout << seconds << "seconds" << std::endl;
-  std::cout << frequency << " commands/second" << std::endl;
+  std::cout << (count - 1) << "," << seconds << "," << frequency << std::endl;
 
   rdx.disconnect();
   return 0;

6b64a065aea848220e6dd273c206096da640df2b
Cristina Teodoropol <c.teodoropol@gmail.com>
2017-07-26 00:26:00 +0000

    Using old googlelog version to work around gflags issue.

--- src/vendor/googlelog.cmake
+++ src/vendor/googlelog.cmake
@@ -2,17 +2,15 @@
 #   - https://github.com/rsakamoto/dexter/blob/13bac2f372bf0beeba673187c39d657978d71890/ext/glog/CMakeLists.txt
 ExternalProject_Add(googlelog_project
     GIT_REPOSITORY "https://github.com/google/glog"
-    GIT_TAG "master"
+    GIT_TAG "v0.3.5"
     PREFIX ${CMAKE_CURRENT_BINARY_DIR}
-    BUILD_IN_SOURCE 1
-    UPDATE_COMMAND ""
-    CONFIGURE_COMMAND ./autogen.sh && ./configure
-    BUILD_COMMAND make
     INSTALL_COMMAND ""
 )
 
-SET(GLOG_INCLUDE_DIRS ${CMAKE_CURRENT_BINARY_DIR}/src/googlelog_project/src)
-SET(GLOG_LINK_DIRS ${CMAKE_CURRENT_BINARY_DIR}/src/googlelog_project/.libs)
+ExternalProject_Get_Property(googlelog_project SOURCE_DIR)
+ExternalProject_Get_Property(googlelog_project BINARY_DIR)
+SET(GLOG_INCLUDE_DIRS ${SOURCE_DIR}/src ${BINARY_DIR})
+SET(GLOG_LINK_DIRS ${BINARY_DIR})
 
 ADD_LIBRARY(googlelog STATIC IMPORTED)
 SET_PROPERTY(TARGET googlelog
